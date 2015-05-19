535509d0622fc50ce1af0fdd9a6e98ef98351f2b
StoneColdSorcery <bradcf90@gmail>
2015-05-19 16:19:22 -0500

    second commit file added

---
+++ uart_atlys_test.py
@@ -0,0 +1,18 @@
+from enum import Enum
+import serial
+import struct
+from time import sleep
+
+head = (0x7E)
+data = (0x0f,0xf0,0x00,0xff) 
+print(head)
+print(data)
+values = (head,len(data)) + data
+msg = struct.pack('<{0}B'.format(len(values)), *values)
+print(msg)
+
+
+sp = serial.Serial('COM8')
+sp.baudrate = 57600
+sp.write(msg)
+sp.close()
